dc0e5d974df4f0615088c0269f22f7de90c65089
Sound <sound@sagaforce.com>
2025-08-04 23:06:11 -0700

    Increase context timeout to 30.

--- pastabin.go
+++ pastabin.go
@@ -308,7 +308,7 @@ func router(w http.ResponseWriter, r *http.Request) {
 
 	mongoURI := fmt.Sprintf("mongodb://%s%s:%d/?authSource=pastabin", mongoUserPassword, mongoHost, mongoPort)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
 	if err != nil {
